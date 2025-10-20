cc0f7467454010a86b1e38f642af41f2307eef08
domiks <ipb24.d.kass@vtdt.edu.lv>
2025-10-20 17:55:21 +0300

    Removed read me

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+import pygame
+from 
 
+#initialize Pygame
+pygame.init()
+
+#asset library
+screen = pygame.display.set_mode((1280, 720))
+
+while True:
+    for event in pygame.event.get():
+       if event.type == pygame.QUIT:
+        pygame.quit()
+    #draw all elements
+    #update everything
+    pygame.display.update()
