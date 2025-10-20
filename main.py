c178f1c3d0a2a2bca50e09287b77cc641eeeee3e
domiks <ipb24.d.kass@vtdt.edu.lv>
2025-10-20 18:16:17 +0300

    Added Game background

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 import pygame
-from 
+from sys import exit
 
 #initialize Pygame
 pygame.init()
 
+#color library
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+black = (0, 0, 0)
+white = (255, 255, 255)
+purple = (127, 0 , 255)
+orange = (255, 165, 0)
+
 #asset library
 screen = pygame.display.set_mode((1280, 720))
+pygame.display.set_caption('Function to Date')
+clock = pygame.time.Clock()
+
+background = pygame.image.load('Assets/FtD_BG.png')
 
 while True:
     for event in pygame.event.get():
        if event.type == pygame.QUIT:
         pygame.quit()
+        exit()
     #draw all elements
     #update everything
     pygame.display.update()
+    clock.tick(60)  # limits FPS to 60
