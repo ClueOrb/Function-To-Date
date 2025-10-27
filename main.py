bd209ac6f0713fbe6eca35facfe62aff4940ef61
domiks <ipb24.d.kass@vtdt.edu.lv>
2025-10-27 19:31:52 +0200

    Added placeholder Monitor

--- main.py
+++ main.py
@@ -4,13 +4,11 @@ from sys import exit
 #initialize Pygame
 pygame.init()
 
-#functions
-   
-
 #main setup (Screen, Title, FPS)
 screen = pygame.display.set_mode((1280, 720), 0)
 pygame.display.set_caption('Function to Date')
-framerate = pygame.time.Clock()
+framerate = 60
+clock = pygame.time.Clock()
 
 #screen assets
 test_surface = pygame.Surface((720, 500))
@@ -20,6 +18,10 @@ background = pygame.image.load("graphic/assets/mainbg.png")
 #variables
 font = pygame.font.Font("freesansbold.ttf", 16)
 
+#functions
+def draw_monitor():
+   pygame.draw.rect(test_surface, "Blue", [250, 250, 200, 170])
+
 while True:
     for event in pygame.event.get():
        if event.type == pygame.QUIT:
@@ -28,6 +30,6 @@ while True:
 
     screen.blit(background,(0, 0))
     screen.blit(test_surface,(60, 60))
-
+    draw_monitor()
     pygame.display.update()
-    framerate.tick(60)  # limits FPS to 60
+    clock.tick(framerate)  # limits FPS to 60
