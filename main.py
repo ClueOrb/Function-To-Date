009ee36f40d30714b21776a3cf4c896ec8f1c6f2
domiks <ipb24.d.kass@vtdt.edu.lv>
2025-10-27 20:01:07 +0200

    Added click funvtion + added each click to increase value

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import time
 from sys import exit
 
 #initialize Pygame
@@ -14,22 +15,40 @@ clock = pygame.time.Clock()
 test_surface = pygame.Surface((720, 500))
 test_surface.fill("Black")
 background = pygame.image.load("graphic/assets/mainbg.png")
+font = pygame.font.Font("freesansbold.ttf", 16)
 
 #variables
-font = pygame.font.Font("freesansbold.ttf", 16)
+monitor_value = 0
+monitor_clicked = False
 
 #functions
 def draw_monitor():
-   pygame.draw.rect(test_surface, "Blue", [250, 250, 200, 170])
+   monitor = pygame.Rect(250, 250, 200, 170)
+   pygame.draw.rect(test_surface, "Blue", monitor)
+   return monitor
 
 while True:
+
+#Closing the window
     for event in pygame.event.get():
        if event.type == pygame.QUIT:
         pygame.quit()
         exit()
 
-    screen.blit(background,(0, 0))
-    screen.blit(test_surface,(60, 60))
-    draw_monitor()
+    if event.type == pygame.MOUSEBUTTONDOWN and monitor_clicked == False:
+       if monitor.collidepoint(event.pos):
+          monitor_value += 1
+          monitor_clicked = True
+    elif event.type == pygame.MOUSEBUTTONUP and monitor_clicked == True:
+          time.sleep(0.05)
+          monitor_clicked = False
+          
+
+    screen.blit(background,(0, 0)) #Draws game screen
+    screen.blit(test_surface,(60, 60)) #Draws game window
+    monitor = draw_monitor() #Draws monitor
+    paperclips = font.render(f"Paperclips: {monitor_value}", 16, "White")
+    screen.blit(paperclips, (1000, 20))
+
     pygame.display.update()
     clock.tick(framerate)  # limits FPS to 60
