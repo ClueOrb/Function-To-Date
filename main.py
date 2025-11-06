cec73b541298e3e09e77980ae773a7deb6fcd407
domiks <ipb24.d.kass@vtdt.edu.lv>
2025-11-06 17:47:36 +0200

    test commit

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
 import pygame, time
 from sys import exit
 
-#----initialize Pygame----#
+#----Initialize Pygame----#
 pygame.init()
 
-#---main set up (Screen, Title, FPS)---#
+#---Main set up (Screen, Title, FPS)---#
 screen = pygame.display.set_mode((1280, 720), 0)
 pygame.display.set_caption('Function to Date')
 framerate = 60
 clock = pygame.time.Clock()
 
-#---screen assets---#
+#---Screen assets---#
 office_surface = pygame.Surface((680, 610))
 office_surface.fill("Red")
 upgrade_surface = pygame.Surface((490, 610))
 upgrade_surface.fill("Blue")
 
-#---variables---#
+#---Variables---#
    #--Fonts--#
 value_font = pygame.font.Font("graphic/font/GoldenAgeShad.ttf", 20)
+upgrade_font = pygame.font.Font("graphic/font/GoldenAgeShad.ttf", 16)
 
-#---functions---#
+#---Functions---#
 
 
 #---Game---#
@@ -34,8 +35,8 @@ while True:
 
 #--Surfaces--#
 
-   screen.blit(office_surface, (50, 50))
-   screen.blit(upgrade_surface, (740, 50))
+   screen.blit(office_surface, (50, 50)) #--Office
+   screen.blit(upgrade_surface, (740, 50)) #--Upgrade store
 
    pygame.display.update()
    clock.tick(framerate)  #limits FPS to 60
